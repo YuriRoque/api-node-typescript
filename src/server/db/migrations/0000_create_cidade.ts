d2aa19c99c935f7d6225165780a4a6cc610c2927
Yuri Roque Benvenutti <yrbenvenutti@gmail.com>
2024-03-05 07:40:26 -0400

    change name to english

--- src/server/db/migrations/0000_create_cidade.ts
+++ src/server/db/migrations/0000_create_cidade.ts
@@ -3,19 +3,19 @@ import { tableNames } from '../tableNames';
 
 export async function up(knex: Knex) {
   knex.schema
-    .createTable(tableNames.cidade, table => {
+    .createTable(tableNames.city, table => {
       table.bigIncrements('id').primary().index();
       table.string('name', 150).index().notNullable();
 
       table.comment('Table used to store cities');
     })
     .then(() => {
-      console.log(`# Create table ${tableNames.cidade}`);
+      console.log(`# Create table ${tableNames.city}`);
     });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable(tableNames.cidade).then(() => {
-    console.log(`# Drop table ${tableNames.cidade}`);
+  return knex.schema.dropTable(tableNames.city).then(() => {
+    console.log(`# Drop table ${tableNames.city}`);
   });
 }
--- src/server/db/tableNames.ts
+++ src/server/db/tableNames.ts
@@ -1,3 +1,3 @@
 export enum tableNames {
-  cidade = 'cidade',
+  city = 'city',
 }
