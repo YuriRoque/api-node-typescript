def3147d6abbe8c35993f29804648752bb3faea7
Yuri Roque Benvenutti <yrbenvenutti@gmail.com>
2024-03-08 11:07:33 -0400

    feat: create city provider

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { City } from '../../db/models/City';
+import { citiesProvider } from '../../db/providers/cities';
 import { validation } from '../../shared/middlewares/Validation';
 
 type BodyProps = Omit<City, 'id'> & {};
@@ -8,11 +9,21 @@ type BodyProps = Omit<City, 'id'> & {};
 export const createCityValidation = validation(getSchema => ({
   body: getSchema<BodyProps>(
     yup.object().shape({
-      name: yup.string().required().min(3),
+      name: yup.string().required().min(3).max(150),
     }),
   ),
 }));
 
 export const createCity = async (req: Request<{}, {}, City>, res: Response) => {
-  return res.status(201).json(1);
+  const result = await citiesProvider.create(req.body);
+
+  if (result instanceof Error) {
+    return res.status(500).json({
+      errors: {
+        default: result.message,
+      },
+    });
+  }
+
+  return res.status(201).json(result);
 };
